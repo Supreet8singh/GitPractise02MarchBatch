b95847f9782d86cac73f4bce77dd2dddd25f1868
Supreet8singh <48771166+Supreet8singh@users.noreply.github.com>
2019-04-13 21:52:56 +0530

    added 1 in front of the amount

--- src/test/Addition.java
+++ src/test/Addition.java
@@ -3,10 +3,10 @@ package test;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a = 1000;
-		int b = 2000;
-		int d = 3000;
-		int e = 5000;
+		int a = 11000;
+		int b = 12000;
+		int d = 13000;
+		int e = 15000;
 		int c = a+b+d+e;
 		System.out.println(c);
 	}
