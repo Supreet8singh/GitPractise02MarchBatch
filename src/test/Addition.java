201241e85456a01d689263fac8b93fa3d2845cbe
Supreet <supreet8singh@gmail.com>
2019-04-13 21:15:27 +0530

    added more variables

--- src/test/Addition.java
+++ src/test/Addition.java
@@ -5,7 +5,9 @@ public class Addition {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
-		int c = a+b;
+		int d = 30;
+		int e = 50;
+		int c = a+b+d+e;
 		System.out.println(c);
 	}
 
